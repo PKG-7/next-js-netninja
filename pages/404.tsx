c46a6a774788a20d909c65d24be50dc8e96f47b4
PKG-7 <pavel.k.group@gmail.com>
2022-12-10 00:39:14 +0300

    7 les ended

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 interface Props {
   //   text: string;
 }
@@ -6,6 +8,10 @@ export default function NotFound({}: Props) {
   return (
     <div className='not-found'>
       <h1>Ooooooopppppsss.....</h1>
+      <h2>That page could not be found</h2>
+      <p>
+        Go back bech <Link href='/'>Home</Link>{' '}
+      </p>
     </div>
   );
 }
